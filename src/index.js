cdea4d350dea35268a33b29bc938d05b9e3c05a6
Hariom Patel <hariom70002582@gmail.com>
2026-01-11 11:12:53 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require("dotenv").config({path: './.env'});
+import dotenv from "dotenv";
+
+import experss from "express";
+import connectDB from "./db/db.js";
+const app = experss();
+dotenv.config({path: './.env'});
+
+connectDB()
+
+/*
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.
+            MONGODB_URL}/${DB_NAME}`);
+            app.on('error',(error)=>{
+                console.log("Error",error);
+                throw error;
+            })
+            
+            app.listen(process.env.PORT,()=>{
+                console.log(`App is listening on port ${process.env.PORT}`);
+            });
+    }catch(error){
+        console.error('Error',error);
+        throw error;
+    }
+})()
+*/
