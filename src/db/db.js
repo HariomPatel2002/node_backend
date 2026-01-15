3d1fa6882e8f384dcf3e0ddc18e7bf9681d4a718
Hariom Patel <hariom70002582@gmail.com>
2026-01-15 21:53:27 +0530

    user Registration

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,6 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () =>{
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        console.log("The connection instance is:",connectionInstance);
         console.log(`\n MongoDB connectedd !! DB HOST:
             ${connectionInstance.connection.host}`);
     }catch(error){
